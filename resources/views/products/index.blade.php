ef73f96d987227e4baaa2ed7b2424dc34d2b7ea7
hassanayn <hassa9kashmal@gmail.com>
2025-10-07 21:18:35 +0300

    feat: display product list in index view with table structure

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,7 +8,30 @@
 </head>
 <body>
     <h1>Product</h1>
-    <div>index</div>
+    <div>
+        <table border="1">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Quantity</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                    <tr>
+                        <td>{{ $product->id }}</td>
+                        <td>{{ $product->name }}</td>
+                        <td>{{ $product->quantity }}</td>
+                        <td>{{ $product->description }}</td>
+                        <td>{{ $product->price }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 
 </body>
 </html>
