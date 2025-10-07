3ba4a0554bbb4d4d6e6c842c0349f6bbabd8b989
hassanayn <hassa9kashmal@gmail.com>
2025-10-07 19:58:56 +0300

    add products create route and create its controller method

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,4 @@ Route::get('/', function () {
 });
 
 Route::get('products', [ProductController::class, 'index'])->name('products.index');
+Route::get('products/create', [ProductController::class, 'create'])->name('products.create');
